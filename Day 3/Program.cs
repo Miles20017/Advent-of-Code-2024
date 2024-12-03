065594a9d2e9d7da38a969b0fa478555b9ac9883
miles <miles@DESKTOP-6CVPE5V>
2024-12-03 05:46:33 +0000

    Day 3: RegEx Part 1 uses the RegEx expression identified by 'pattern' to detect mul() statements. Part 2 uses the RegEx expression identified by 'Enabler' to detect enabiling and disabiling statements do() and don't(), respectivly.

---
+++ Day 3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace Day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> toMultiply = new List<int>();
+            List<string> Possible = new List<string>();
+            
+
+            string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+            Regex regex = new Regex(pattern);
+
+            string Enabler = @"do\(\)|don't\(\)"; 
+            Regex regex2 = new Regex(Enabler);
+
+            bool Enabled = true;
+
+            string path = "day3.txt";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string input;
+                input = reader.ReadToEnd();
+
+                MatchCollection matches = regex.Matches(input);
+                MatchCollection DoDontMatch = regex2.Matches(input);
+
+                for(int i = 0; i < input.Length; i++)
+                {
+                    foreach(Match match in DoDontMatch)
+                    {
+                        if (i == match.Index)
+                        {
+                            if (match.Value == "don't()")
+                            {
+                                Enabled = false;
+                            }
+                            else
+                            {
+                                Enabled = true;
+                            }
+                        }
+                    }
+
+                    foreach(Match match in matches)
+                    {
+                        if (Enabled)
+                        {
+                            if (i == match.Index)
+                            {
+                                Possible.Add(match.Value);
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach(string command in Possible)
+            {
+                string ToConvert = "";
+                for(int i = 4; i < command.Length; i++)
+                {
+                    while (command[i] != ',')
+                    {
+                        ToConvert+= command[i];
+                        i++;
+                    }
+                    i++;
+                    toMultiply.Add(int.Parse(ToConvert));
+                    ToConvert = "";
+                    while (command[i] != ')')
+                    {
+                        ToConvert += command[i];
+                        i++;
+                    }
+                    toMultiply.Add(int.Parse(ToConvert));
+                }
+            }
+
+            int sum = 0;
+            for(int i = 0; i < toMultiply.Count()-1; i+=2)
+            {
+                sum+=(toMultiply[i] * toMultiply[i + 1]);
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
