176f3e667ed974fd659c303f979a9a5a4ca4aa8e
miles <miles@MILESLAPTOP>
2024-12-06 14:11:35 +0000

    Day 2 - Completed This is my second attempt and it finally works, just 9 hours after the puzzle dropped. There were a lot of issues with the x and y coordinates being swapped and I'm still not too sure where the issue lies but after some messing around with the code it works. It finds possible loops and a number of infinite loops. The soltuion to the problem is the possible loops + the inifinie loops.

--- Day 6 - Attempt 2/Program.cs
+++ Day 6 - Attempt 2/Program.cs
@@ -9,6 +9,31 @@ namespace Day_6___Attempt_2
 {
     internal class Program
     {
+        static int LoopCounter = 0;
+        static int LoopingError = 0;
+
+        static bool FoundLoop(point node, string Direction)
+        {
+            if (node.Direction == Direction && node.c == 'X')
+            {
+                Console.WriteLine("Loop Found!");
+                return true;
+            }
+            return false;
+        }
+
+        static void DrawTestGrid(point[,] map)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                for(int y = 0; y < map.GetLength(0); y++)
+                {
+                    Console.Write(map[y, x].c); 
+                }
+                Console.WriteLine();
+            }
+        }
+
         struct point
         {
             public char c;
@@ -27,24 +52,32 @@ namespace Day_6___Attempt_2
 
         static void DrawPath(point[,] map, coordinate startPoint)
         {
+            int PathLength = 0;
             string Direction = "UP";
 
             //Values change based on direction, Starting up => y value decreases with each move, hence = -1
             coordinate VectorToAdd = new coordinate();
-            VectorToAdd.x = 0;
-            VectorToAdd.y = -1;
+            VectorToAdd.y = 0;
+            VectorToAdd.x = -1;
 
-            int x = startPoint.x;
-            int y = startPoint.y;
+            int x = startPoint.y;
+            int y = startPoint.x;
 
-            while (WithinBounds(map, x, y))
+            while (WithinBounds(map, x, y)&&PathLength<10000)
             {
-                while (WithinBounds(map, x, y) && map[y, x].c != '#')
+                while (WithinBounds(map, x, y) && (map[y, x].c != '#') && PathLength < 10000)
                 {
+                    if (FoundLoop(map[y,x],Direction)){
+                        LoopCounter++;
+                        return;
+                    }
                     map[y, x].c = 'X';
                     map[y, x].Direction = Direction;
+                    PathLength++;
                     x += VectorToAdd.x;
                     y += VectorToAdd.y;
+                    //DrawTestGrid(map);
+                    //Console.ReadKey();
                 }
 
                 //We moved our pointer onto a # so we need to move it back before turning
@@ -54,32 +87,35 @@ namespace Day_6___Attempt_2
                     map[y, x].Direction = null;
                     x -= VectorToAdd.x;
                     y -= VectorToAdd.y;
+                    //DrawTestGrid(map);
+                    //Console.ReadKey();
+                    //Console.Clear();
                 }
 
                 switch (Direction)
                 {
                     case "UP":
                         //Vector for moving right
-                        VectorToAdd.x = 1;
-                        VectorToAdd.y = 0;
+                        VectorToAdd.y = 1;
+                        VectorToAdd.x = 0;
                         Direction = "RIGHT";
                         break;
                     case "DOWN":
                         //Vector for moving left
-                        VectorToAdd.x = -1;
-                        VectorToAdd.y = 0;
+                        VectorToAdd.y = -1;
+                        VectorToAdd.x = 0;
                         Direction = "LEFT";
                         break;
                     case "LEFT":
                         //Vector for moving up
-                        VectorToAdd.x = 0;
-                        VectorToAdd.y = -1;
+                        VectorToAdd.y = 0;
+                        VectorToAdd.x = -1;
                         Direction = "UP";
                         break;
                     case "RIGHT":
                         //Vector for moving down
-                        VectorToAdd.x = 0;
-                        VectorToAdd.y = 1;
+                        VectorToAdd.y = 0;
+                        VectorToAdd.x = 1;
                         Direction = "DOWN";
                         break;
                     default:
@@ -87,6 +123,12 @@ namespace Day_6___Attempt_2
                         Console.WriteLine($"Direction Value: {Direction}");
                         break;
                 }
+            }
+
+            if (PathLength > 9999)
+            {
+                Console.WriteLine("Looping Error");
+                LoopingError++;
             }
         }
 
@@ -97,37 +139,57 @@ namespace Day_6___Attempt_2
             string path = "day6.txt";
             using (StreamReader reader = new StreamReader(path))
             {
+                Console.WriteLine("Text File");
                 while (!reader.EndOfStream)
                 {
                     Lines.Add(reader.ReadLine());
+                    Console.WriteLine(Lines[Lines.Count() - 1]);
                 }
+                Console.WriteLine();
             }
 
             point[,] Map = new point[Lines.Count(), Lines[0].Count()];
             coordinate StartPos = new coordinate();
             bool FoundStartPos = false;
 
+            Console.WriteLine("2D Array Equivelant");
             for(int x = 0; x < Lines[0].Count(); x++)
             {
                 for(int y = 0; y < Lines.Count(); y++)
                 {
-                    Map[y, x].c = Lines[y][x];
+                    Map[y, x].c = Lines[x][y];
+                    Console.Write(Map[y, x].c);
 
                     if (Lines[y][x] == '^')
                     {
                         StartPos.x = x;
                         StartPos.y = y;
                         FoundStartPos = true;
-                        Console.WriteLine($"Guard Start Point: ({StartPos.x},{StartPos.y})");
+
                     }
                 }
+
+                Console.WriteLine();
             }
 
             if(!FoundStartPos) Console.WriteLine($"Error Code 2: Failed To Find Guard Start Position. Using Start Position ({StartPos.x},{StartPos.y})");
-
+            else
+            {
+                Console.WriteLine($"Guard Start Point: ({StartPos.x},{StartPos.y})");
+            }
             //Now we have a 2D array called Map, where each character has a direction (instantiated at null)
+            point[,] CleanMap = new point[Lines.Count(), Lines[0].Count()];
+            for (int x = 0; x < Lines[0].Count(); x++)
+            {
+                for (int y = 0; y < Lines.Count(); y++)
+                {
+                    CleanMap[y, x] = Map[y, x];
+                }
+            }
 
             int counter = 0;
+            Console.ReadKey();
+            Console.Clear();
             DrawPath(Map, StartPos);
             for (int x = 0; x < Lines[0].Count(); x++)
             {
@@ -140,7 +202,36 @@ namespace Day_6___Attempt_2
                 }
             }
 
+            //Some gross infinite Loop in here somewhere
+            for (int x = 0; x < Lines[0].Count(); x++)
+            {
+                for (int y = 0; y < Lines.Count(); y++)
+                {
+                    point[,] AugamentedMap = new point[Lines.Count(), Lines[0].Count()];
+                    for (int i = 0; i < Lines[0].Count(); i++)
+                    {
+                        for (int j = 0; j < Lines.Count(); j++)
+                        {
+                            AugamentedMap[j, i] = CleanMap[j, i];
+                        }
+                    }
+                    //Console.Clear();
+                    //DrawTestGrid(AugamentedMap);
+                    //Console.ReadKey();
+                    Console.WriteLine("Copied Array Successfully");
+
+                    if ((AugamentedMap[y, x].c != '#') && (AugamentedMap[y, x].c != '^'))
+                    {
+                        Console.WriteLine("Remooved a point!");
+                        AugamentedMap[y, x].c = '#';
+                        DrawPath(AugamentedMap, StartPos);
+                    }
+                }
+            }
+
             Console.WriteLine($"Unique Positions For Guard: {counter}");
+            Console.WriteLine($"Number of possible loops: {LoopCounter}");
+            Console.WriteLine($"With {LoopingError} Infinite Loops");
             Console.ReadKey();
         }
     }
