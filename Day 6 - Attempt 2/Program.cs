3f2698e08a18eac41aa4e4809a020c201a24d605
miles <miles@MILESLAPTOP>
2024-12-06 10:08:05 +0000

    Added Error Codes, Current Bug: Guard Start Position can't be found.

--- Day 6 - Attempt 2/Program.cs
+++ Day 6 - Attempt 2/Program.cs
@@ -39,51 +39,53 @@ namespace Day_6___Attempt_2
 
             while (WithinBounds(map, x, y) && map[y, x].c != '#')
             {
-                while (WithinBounds(map, x, y) && map[y, x].c != '#')
-                {
-                    map[y, x].c = 'X';
-                    map[y,x].Direction = Direction;
-                    x += VectorToAdd.x;
-                    y += VectorToAdd.y;
-                }
-
-                //We moved our pointer onto a # so we need to move it back before turning
-                if (WithinBounds(map, x, y))
-                {
-                    map[y, x].c = '#';
-                    map[y, x].Direction = null;
-                    x -= VectorToAdd.x;
-                    y -= VectorToAdd.y;
-                }
-
-                switch (Direction)
-                {
-                    case "UP":
-                        VectorToAdd.x = 0;
-                        VectorToAdd.y = -1;
-                        Direction = "RIGHT";
-                        break;
-                    case "DOWN":
-                        VectorToAdd.x = 0;
-                        VectorToAdd.y = 1;
-                        Direction = "LEFT";
-                        break;
-                    case "LEFT":
-                        VectorToAdd.x = -1;
-                        VectorToAdd.y = 0;
-                        Direction = "UP";
-                        break;
-                    case "RIGHT":
-                        VectorToAdd.x = 1;
-                        VectorToAdd.y = 0;
-                        Direction = "DOWN";
-                        break;
-                    default:
-                        Console.WriteLine("Error Code 1: Direction value is not recognisde by program");
-                        Console.WriteLine($"Direction Value: {Direction}");
-                        break;
-                }
+                map[y, x].c = 'X';
+                map[y,x].Direction = Direction;
+                x += VectorToAdd.x;
+                y += VectorToAdd.y;
             }
+
+            //We moved our pointer onto a # so we need to move it back before turning
+            if (WithinBounds(map, x, y))
+            {
+                map[y, x].c = '#';
+                map[y, x].Direction = null;
+                x -= VectorToAdd.x;
+                y -= VectorToAdd.y;
+            }
+
+            switch (Direction)
+            {
+                case "UP":
+                    //Vector for moving right
+                    VectorToAdd.x = 1;
+                    VectorToAdd.y = 0;
+                    Direction = "RIGHT";
+                    break;
+                case "DOWN":
+                    //Vector for moving left
+                    VectorToAdd.x = -1;
+                    VectorToAdd.y = 0;
+                    Direction = "LEFT";
+                    break;
+                case "LEFT":
+                    //Vector for moving up
+                    VectorToAdd.x = 0;
+                    VectorToAdd.y = -1;
+                    Direction = "UP";
+                    break;
+                case "RIGHT":
+                    //Vector for moving down
+                    VectorToAdd.x = 0;
+                    VectorToAdd.y = 1;
+                    Direction = "DOWN";
+                    break;
+                default:
+                    Console.WriteLine("Error Code 1: Direction value is not recognisde by program");
+                    Console.WriteLine($"Direction Value: {Direction}");
+                    break;
+            }
+            
         }
 
         static void Main(string[] args)
@@ -98,6 +100,7 @@ namespace Day_6___Attempt_2
 
             point[,] Map = new point[Lines.Count(), Lines[0].Count()];
             coordinate StartPos = new coordinate();
+            bool FoundStartPos = false;
 
             for(int x = 0; x < Lines[0].Count(); x++)
             {
@@ -105,13 +108,17 @@ namespace Day_6___Attempt_2
                 {
                     Map[y, x].c = Lines[y][x];
 
-                    if (Map[y, x].c == '^')
+                    if (Lines[y][x] == '^')
                     {
                         StartPos.x = x;
                         StartPos.y = y;
+
+                        Console.WriteLine($"Guard Start Point: ({StartPos.x},{StartPos.y})");
                     }
                 }
             }
+
+            if(!FoundStartPos) Console.WriteLine($"Error Code 2: Failed To Find Guard Start Position. Using Start Position ({StartPos.x},{StartPos.y})");
 
             //Now we have a 2D array called Map, where each character has a direction (instantiated at null)
 
