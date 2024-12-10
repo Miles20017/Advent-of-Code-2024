d45d64291175cbbc8a630218fb83b5600de39333
miles <miles@DESKTOP-6CVPE5V>
2024-12-10 17:55:12 +0000

    Day 10 - Completed

--- Day 10 - Attempt 2/Program.cs
+++ Day 10 - Attempt 2/Program.cs
@@ -58,9 +58,86 @@ namespace Day_10___Attempt_2
             return map;
         }
 
-        static int DFS(Node[,] map, Node CurrentNode, int Target)
+        static List<Node> Adjacent(Node[,] map, Node node)
         {
+            List<Node> adjacent = new List<Node>();
 
+            int y = node.GetCoordinates().Item1;
+            int x = node.GetCoordinates().Item2;
+
+            try
+            {
+                adjacent.Add(map[y - 1 , x]);
+            }
+            catch { }
+            try
+            {
+                adjacent.Add(map[y + 1 , x]);
+            }
+            catch { }
+            try
+            {
+                adjacent.Add(map[y, x - 1]);
+            }
+            catch { }
+            try
+            {
+                adjacent.Add(map[y, x + 1]);
+            }
+            catch { }
+
+
+            return adjacent;
+        }
+
+        static void DFS(Node[,] map, Node CurrentNode, int Target, ref int count)
+        {
+            CurrentNode.visited = true;
+
+            if(CurrentNode.value == Target)
+            {
+                count++;
+            }
+            else
+            {
+                foreach(Node node in Adjacent(map, CurrentNode))
+                {
+                    if((!node.visited) && (node.value - CurrentNode.value) == 1)
+                    {  
+                        DFS(map, node, Target,ref count);
+                    }
+                }
+            }
+        }
+
+        static int  DFS2(Node[,] map, Node CurrentNode, int Target)
+        {
+            CurrentNode.visited = true;
+
+            if (CurrentNode.value == Target)
+            {
+                return 1;
+            }
+            else
+            {
+                int total = 0;
+                foreach (Node node in Adjacent(map, CurrentNode))
+                {
+                    if ((node.value - CurrentNode.value) == 1)
+                    {
+                        total+=DFS2(map, node, Target);
+                    }
+                }
+                return total;
+            }
+        }
+
+        static void ResetFlags(Node[,] map)
+        {
+            foreach(Node node in map)
+            {
+                node.visited = false;
+            }
         }
 
         static int FindStartNodes(Node[,] map)
@@ -71,16 +148,24 @@ namespace Day_10___Attempt_2
             {
                 if (node.value == 0)
                 {
-                    Score += 
+                    int CurrentSearch = 0;
+                    //DFS(map, node, 9, ref CurrentSearch);
+                    CurrentSearch = DFS2(map, node, 9);
+                    ResetFlags(map);
+                    Score += CurrentSearch;
+                    Console.WriteLine(CurrentSearch);
                 }
             }
+
+            return Score;
         }
 
         static void Main(string[] args)
         {
-            Node[,] map = ConvertTextFile("day10test.txt");
+            Node[,] map = ConvertTextFile("day10.txt");
 
-
+            Console.WriteLine($"Score: {FindStartNodes(map)}");
+            Console.ReadKey();
         }
     }
 }
